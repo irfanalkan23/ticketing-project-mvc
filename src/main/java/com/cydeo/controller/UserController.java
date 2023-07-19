b76cfa38faa41c4634f4d5cafe14a1fce0f0646e
irfan <23pointalpha@gmail.com>
2023-07-19 09:56:01 +0300

    part-2 User List table done

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -27,6 +27,7 @@ public class UserController {
     public String createUser(Model model){
         model.addAttribute("user",new UserDTO());
         model.addAttribute("roles", roleService.findAll());      //bring me all roles from DB
+        model.addAttribute("users", userService.findAll());
         return "user/create";
     }
 }
